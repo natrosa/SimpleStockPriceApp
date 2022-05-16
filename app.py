3f47ff77d8544b1b4cc2d516ba5cc2fa860b9f9c
Natasha Rosa <natrosdev12@gmail.com>
2022-05-15 21:39:18 -0400

    added support for crypto

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import datetime #for query
 
 # App title
 st.markdown('''
-# Simple Stock Price App
-**Credits**
-- App built folliwing [Tutorial](https://www.youtube.com/watch?v=0pHJOzNDdOo) by [Data Professor](http://youtube.com/dataprofessor)
+# Simple Stock/Crypto Price App
 ''')
 st.write('---')
 
@@ -20,19 +18,31 @@ start_date = st.sidebar.date_input("Start date", datetime.date(2020, 1, 1))
 end_date = st.sidebar.date_input("End date", datetime.date(2022, 1, 31))
 
 # Retrieving data
-tickerSymbol = st.text_input("Enter stock symbol")
+st.title("Enter stock/crypto symbol: ")
+tickerSymbol = st.text_input("Example: AMD, TWTR, AAPL, TSLA, FB, BTC-USD, ETH-USD")
 tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get the historical prices for this ticker
 
 # Ticker information
-string_logo = '<img src=%s>' % tickerData.info['logo_url']
-st.markdown(string_logo, unsafe_allow_html=True)
+stock_logo = '<img src=%s>' % tickerData.info['logo_url']
+st.markdown(stock_logo, unsafe_allow_html=True)
 
-string_name = tickerData.info['longName']
-st.header('**%s**' % string_name)
+if "longName" in tickerData.info:
+    stock_name = tickerData.info['longName']
+    st.header('**%s**' % stock_name)
+else:
+    if "shortName" in tickerData.info:
+        stock_name = tickerData.info['shortName'] #for crypto
+        st.header('**%s**' % stock_name)
 
-string_summary = tickerData.info['longBusinessSummary']
-st.info(string_summary)
+
+if "longBusinessSummary" in tickerData.info:
+    stock_summary = tickerData.info['longBusinessSummary']
+    st.write(stock_summary)
+else:
+    if "description" in tickerData.info:
+        stock_summary = tickerData.info['description'] #for crypto
+        st.write(stock_summary)
 
 # Ticker data
 st.header('**Ticker data**')
@@ -47,5 +57,5 @@ st.plotly_chart(fig)
 
 ####
 st.write('---')
-st.header('**Data**')
+st.header('** Raw Data**')
 st.write(tickerData.info)
