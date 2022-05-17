24e43d796a597d34c2f393bdb40198a0d44ae074
Natasha Rosa <natrosdev12@gmail.com>
2022-05-16 20:09:42 -0400

    adding support for prophet

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import pandas as pd #display dataframe
 import cufflinks as cf #for plots 
 import datetime #for query 
 
+#for forecasting
+from datetime import date
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
 # App title
 st.markdown('''
 # Simple Stock/Crypto Price App
@@ -54,6 +60,62 @@ qf=cf.QuantFig(tickerDf,title='First Quant Figure',legend='top',name='GS')
 qf.add_bollinger_bands()
 fig = qf.iplot(asFigure=True)
 st.plotly_chart(fig)
+
+
+#Forecasting 
+st.header('**Forecasting with Prophet**')
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
+@st.cache
+def load_data_forecasting(ticker):
+    data = yf.download(ticker, start_date, end_date)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data_forecasting(tickerSymbol)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
+plot_raw_data()
+
+
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
+
+
+
+
 
 ####
 st.write('---')
